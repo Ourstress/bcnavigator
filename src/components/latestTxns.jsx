2487ba78dc4b3ce4c938e4e2c681d90a6940f3ff
SG <sgng1@e.ntu.edu.sg>
2020-11-23 13:54:21 +0800

    display the 10 most recent blocks

--- src/components/latestTxns.jsx
+++ src/components/latestTxns.jsx
@@ -3,9 +3,21 @@ import Card from "react-bootstrap/Card";
 import useLatestInfo from "../hooks/useLatestInfo";
 
 function LatestTxns() {
-  const latestBlock = useLatestInfo();
+  const [recentBlocks, latestBlock] = useLatestInfo();
   return (
     <>
+      <h3>Recent Blocks</h3>
+      <p>Refreshed every 13s</p>
+      <Container fluid className="card-columns">
+        {recentBlocks &&
+          recentBlocks.map((block, index) => (
+            <Card style={{ width: "12rem" }} key={index}>
+              <Card.Body>
+                <Card.Text>{block.number}</Card.Text>
+              </Card.Body>
+            </Card>
+          ))}
+      </Container>
       <h3>Recent transactions</h3>
       <p>Refreshed every 13s</p>
       <Container fluid className="card-columns">
--- src/hooks/useLatestInfo.js
+++ src/hooks/useLatestInfo.js
@@ -3,14 +3,39 @@ import { Web3Context } from "./web3";
 
 export default function useLatestInfo() {
   const [latestBlock, setLatestBlock] = useState({});
+  const [recentBlocks, setRecentBlocks] = useState([]);
   const web3 = useContext(Web3Context);
   useEffect(() => {
+    const getRecentBlocks = async function () {
+      if (web3) {
+        const latest = await web3.eth.getBlockNumber();
+        const blockNumbers = Array.from(Array(10)).map((_, index) => {
+          index++;
+          return latest - index;
+        });
+        const blocks = [];
+        const addToBlock = function (err, result) {
+          if (err) console.log(err);
+          blocks.push(result);
+        };
+        const batch = new web3.eth.BatchRequest();
+        blockNumbers.forEach((blockNumber) => {
+          batch.add(web3.eth.getBlock.request(blockNumber, addToBlock));
+        });
+
+        batch.execute();
+        setRecentBlocks(blocks);
+      } else {
+        return [];
+      }
+    };
+
     const getLatestTxns = () =>
       web3
         ? web3.eth.getBlock("latest").then((block) => setLatestBlock(block))
         : {};
     getLatestTxns();
-
+    getRecentBlocks();
     // setInterval to refresh latest block every 13s
     const interval = setInterval(() => {
       getLatestTxns();
@@ -18,5 +43,5 @@ export default function useLatestInfo() {
     return () => clearInterval(interval);
   }, [web3]);
   console.log("latestBlock", latestBlock);
-  return latestBlock;
+  return [recentBlocks, latestBlock];
 }
